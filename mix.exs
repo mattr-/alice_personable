64345adf00f5185db33b683c4dd2b73063dc436d
Matt Rogers <mrogers@teladoc.com>
2016-05-24 14:44:58 -0500

    Release v0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AlicePersonable.Mixfile do
 
   def project do
     [app: :alice_personable,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
